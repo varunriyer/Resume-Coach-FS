26c1ad290d89900f5013308fca6ab3fb9909cc73
varunriyer <rvaruniyer@gmail.com>
2025-07-08 12:37:22 +0530

    feat: add AnalysisComponent with HTML, CSS, and test files

---
+++ frontend/Resume_Coach_Frontend/src/app/pages/analysis/analysis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis',
+  imports: [],
+  templateUrl: './analysis.component.html',
+  styleUrl: './analysis.component.css'
+})
+export class AnalysisComponent {
+
+}
