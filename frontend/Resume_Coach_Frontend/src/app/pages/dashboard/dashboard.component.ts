3aab82f3c65239404affde8678b94483466c09d6
varunriyer <rvaruniyer@gmail.com>
2025-07-08 16:33:01 +0530

    feat: implement dashboard layout with navigation and styling

--- frontend/Resume_Coach_Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/Resume_Coach_Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    RouterLink,
+    MatTooltipModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  selected: string = 'home';
+
+  onSelect(tab: string): void {
+    this.selected = tab;
+  }
+}
