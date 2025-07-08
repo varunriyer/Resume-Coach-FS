14b961485b10a55b2ce895c5be3c504c712a39e6
varunriyer <rvaruniyer@gmail.com>
2025-07-08 10:01:29 +0530

    feat: define application routes and set up HomeComponent

--- frontend/Resume_Coach_Frontend/src/app/app.routes.ts
+++ frontend/Resume_Coach_Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
+];
