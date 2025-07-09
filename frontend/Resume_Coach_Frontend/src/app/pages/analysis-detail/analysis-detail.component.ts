3e7b3e5aa8150c4cc33356c363aac740a861710a
varunriyer <rvaruniyer@gmail.com>
2025-07-09 18:34:34 +0530

    feat: implement analysis detail component with query parameter handling and navigation logic

--- frontend/Resume_Coach_Frontend/src/app/pages/analysis-detail/analysis-detail.component.ts
+++ frontend/Resume_Coach_Frontend/src/app/pages/analysis-detail/analysis-detail.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-analysis-detail',
   imports: [],
   templateUrl: './analysis-detail.component.html',
-  styleUrl: './analysis-detail.component.css'
+  styleUrl: './analysis-detail.component.css',
 })
-export class AnalysisDetailComponent {
+export class AnalysisDetailComponent implements OnInit {
+  analysisData: any = null;
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const key = params['key'];
+      if (key) {
+        const data = localStorage.getItem(key);
+        if (data) {
+          this.analysisData = JSON.parse(data);
+        } else {
+          this.router.navigate(['home/analysis']);
+        }
+      } else {
+        this.router.navigate(['home/analysis']);
+      }
+    });
+  }
 }
